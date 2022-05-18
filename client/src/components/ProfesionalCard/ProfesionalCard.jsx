7ecc67777d26b0d524abf12e50b2a4db3f8ce270
AlejandroByMacBookPro18 <alejandro.david.benolol@gmail.com>
2022-05-18 10:30:16 +0300

    borre un par de iconos residuales de react que estaban en public, removi un par de import react y arregle el css de profesionalCard que no se aplicaba porque faltaba el className

--- client/src/components/ProfesionalCard/ProfesionalCard.jsx
+++ client/src/components/ProfesionalCard/ProfesionalCard.jsx
@@ -5,7 +5,7 @@ export default function ProfesionalCard({props}) {
     const { nombre, profesion, imagen } = props
 
     return (
-        <div ProfesionalCard>    
+        <div className='ProfesionalCard'>    
             <h3>{nombre}</h3>
             <img className='CarouselImg' src={require(`../SlideShow/img/${imagen}`)} alt={nombre} />
             <p>{profesion}</p>
--- client/src/components/Titles/Titles.jsx
+++ client/src/components/Titles/Titles.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import './Titles.css'
 
 export default function Titles({ text }) {
